a529dd840ae9f61d77c91f645cd3c682a26d8107
Kdhere <gamelover9899@gmail.com>
2022-04-01 19:20:29 +0530

    Seraching in Python

---
+++ Python/Search.py
@@ -0,0 +1,21 @@
+#Here doing search in list using sepcial feauters of if statement
+import sys
+
+nu=[4,5,56,256,32,0,4]
+#See how we are using while loop here for seraching 
+if 0 in nu:
+    print("Got it")
+    sys.exit(0)
+else:
+    print("Didn't got it")
+    sys.exit(1)
+
+# import sys
+
+# names=['Kd',"Y","dfj",'fh']
+# if "kdd" in names:
+#     print("We got it")
+#     sys.exit(0) 
+# else:
+#     print("NOt got it")
+#     sys.exit(1)
