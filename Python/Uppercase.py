6e398fe875061d3c70945def08f68ecca84c6552
Kdhere <gamelover9899@gmail.com>
2022-04-01 15:02:08 +0530

    Uppercasing of character

---
+++ Python/Uppercase.py
@@ -0,0 +1,24 @@
+# Here we are learning how to upper case in Python
+
+
+
+
+# 1) Using for loop and ascii value
+#ord function is used to return the ASCII value of characters
+#chr function is used to return the ascii character of valve
+
+pahila=input("Enter the string:-")
+for a in pahila:
+    badka=ord(a)-32
+    pal=chr(badka)
+    print(pal,end="")
+print("\nBye")
+
+
+# 2) Using Upper method!
+# upper function is used to make upper case of character
+
+
+# before=input("Enter THe strings:-")
+# after=before.upper()
+# print(after)
