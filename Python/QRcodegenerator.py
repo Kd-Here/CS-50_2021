faae02cfebe60b5602e31a088f1ab5f5d9f52998
Kdhere <gamelover9899@gmail.com>
2022-05-02 15:37:39 +0530

    Web_Dev_Recap

--- Python/QRcodegenerator.py
+++ Python/QRcodegenerator.py
@@ -1,12 +1,15 @@
-# You can visit https://pypi.org/project/qrcode/#description
-# For checking the documentation
+# # You can visit https://pypi.org/project/qrcode/#description
+# # For checking the documentation
 
 import qrcode
-kd=qrcode.make("You can visit https://pypi.org/project/qrcode/#description"
-                "import qrcode"
-                "For checking the documentation"
-                "kd=qrcode.make('')"
-                "type(kd)"
-                "kd.save(Generated Qr_code.png)")
-type(kd)
+qr = qrcode.QRCode(
+         version=1,
+          error_correction=qrcode.constants.ERROR_CORRECT_L,
+          box_size=10,
+        border=4,
+ )
+ad=input("Enter what you wanted to get in QR code scan")
+qr.add_data(ad)
+qr.make(fit=True)
+kd=qr.make_image(fill_color="black", back_color="white")
 kd.save("Generated Qr_code.png")
