1f0eabc2a63c9328e0166a9f8d187304524f63c5
Kd-Here <gamelover9899@gmail.com>
2022-06-20 10:15:51 +0530

    Stanford application

--- Flask/FroshChampion/app.py
+++ Flask/FroshChampion/app.py
@@ -1,14 +1,28 @@
 from flask import Flask,  render_template, request
 app=Flask(__name__)
 
+
+
+#Every time you see copy paste or equivalent to it rememeber you can do better
+#So not in ["Foo..."] we are making global
+SPORTS ={   
+    "Football",
+    "Volleyball",
+    "Baseball",
+    "Bat-ball"
+}
+s=list(SPORTS)
+s.sort()
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html",sports=s)
+ 
 @app.route("/register", methods=["POST"])
 def greet(): 
     #validate submission
-    if not request.form.get("name") or request.form.get("sport") not in ["Football", "Baseball", "Volleyball"]:
+    if not request.form.get("name") or request.form.get("sport") not in s:
         return render_template("Failure.html")
     
     #Confirm Regristration
