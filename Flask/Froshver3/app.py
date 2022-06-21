b37b256b0477bb119272f2bd3c377180a8e76744
Kd-Here <gamelover9899@gmail.com>
2022-06-21 20:49:30 +0530

    T

--- Flask/Froshver3/app.py
+++ Flask/Froshver3/app.py
@@ -31,12 +31,12 @@ def register():
 
 
     #Validating sport
-    sport=reques.form.get("sport")
-    if not sport: #simply means if sport is not filled
+    sport = request.form.get("sport")
+    if not s: #simply means if sport is not filled
         return render_template("error.html", message="Missing Sport")
     
     if sport not in s: #simply means sport nsel s madhey tarch
-        return render_template("errorh.html", message="Invalid Sport")
+        return render_template("error.html", message="Invalid Sport")
 
 
     #Storing User name
@@ -44,4 +44,19 @@ def register():
      
     #Confirm registration
     return redirect("/registrants")
-    #1.19.30
+
+
+
+@app.route("/registrants")
+def regost():
+    return render_template("registrants.html",registrants=REGISTRANTS,ad=i)
+
+# This to keep count on registrants
+# a=len(REGISTRANTS.keys())
+# print(f'The value of keys occured or dictionray iterated is{a}')
+# print(REGISTRANTS)
+# Not working since we are not adding key value to dictionary here
+
+# for i in range(1,1000):
+    # find way to run loop when @app.route ("/registrants") is exceuted that time excute loop
+    # use if app... excute i+1,
